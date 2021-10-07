a92c0b73e24dc1031312ceaeee3af9bc2aa0b7ef
RockTeaGames <42101888+RockTeaGames@users.noreply.github.com>
2021-10-07 16:53:23 -0700

    break apart files

---
+++ premium_tasting.js
@@ -0,0 +1,85 @@
+class Timer {
+    constructor(root) {
+        this.el = {
+            minutes: document.querySelector(".timer__part--minutes"),
+            seconds: document.querySelector(".timer__part--seconds"),
+            control: document.querySelector(".timer__btn--control"),
+            reset: document.querySelector(".timer__btn--reset")
+        };
+        //console.log(this.el);
+        this.interval = null;
+        this.remainingSeconds = 0;
+
+        this.el.control.addEventListener("click", () => {
+            if (this.interval === null) {
+                this.start();
+            } else {
+                this.stop();
+            }
+        });
+
+        this.el.reset.addEventListener("click", () => {
+            var inputMinutes = document.querySelector(".timer__input").value;
+
+            if (inputMinutes < 60) {
+                this.stop();
+                this.remainingSeconds = inputMinutes * 60;
+                this.updateInterfaceTime();
+            }
+        });
+
+        document.querySelector(".new_link_btn").addEventListener("click", () => {
+            document.querySelector(".embed_video").src = document.querySelector(".new_link_input")
+            .value;
+        });
+    }
+
+    updateInterfaceTime() {
+        const minutes = Math.floor(this.remainingSeconds / 60);
+        const seconds = this.remainingSeconds % 60;
+
+        this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+        this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+    }
+
+    updateInterfaceControls() {
+        if (this.interval === null) {
+            this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+            this.el.control.classList.add("timer__btn--start");
+            this.el.control.classList.remove("timer__btn--stop");
+        } else {
+            this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+            this.el.control.classList.add("timer__btn--stop");
+            this.el.control.classList.remove("timer__btn--start");
+        }
+    }
+
+    start() {
+        if (this.remainingSeconds === 0) return;
+        this.interval = setInterval(() => {
+            this.remainingSeconds--;
+            this.updateInterfaceTime();
+
+            if (this.remainingSeconds === 0) {
+                this.stop();
+                this.play();
+            }
+        }, 1000);
+        this.updateInterfaceControls();
+    }
+
+    stop() {
+        clearInterval(this.interval);
+        this.interval = null;
+        this.updateInterfaceControls();
+    }
+
+    play() {
+        var audio = new Audio("./alarm.wav");
+        audio.play();
+    }
+}
+
+function start_timer(){
+    new Timer(document.querySelector(".timer"));
+}
