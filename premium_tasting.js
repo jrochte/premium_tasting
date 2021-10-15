a3cd4a7bc8e443061586e78ca6f0584f08eeb817
RockTeaGames <42101888+RockTeaGames@users.noreply.github.com>
2021-10-15 13:00:59 -0700

    greyout and save link

--- premium_tasting.js
+++ premium_tasting.js
@@ -1,85 +1,35 @@
-class Timer {
-    constructor(root) {
-        this.el = {
-            minutes: document.querySelector(".timer__part--minutes"),
-            seconds: document.querySelector(".timer__part--seconds"),
-            control: document.querySelector(".timer__btn--control"),
-            reset: document.querySelector(".timer__btn--reset")
-        };
-        //console.log(this.el);
-        this.interval = null;
-        this.remainingSeconds = 0;
+var saved_link;
+//var darkMode;
 
-        this.el.control.addEventListener("click", () => {
-            if (this.interval === null) {
-                this.start();
-            } else {
-                this.stop();
-            }
-        });
+function site_onLoad() {
+    new Timer(document.querySelector(".timer"));
 
-        this.el.reset.addEventListener("click", () => {
-            var inputMinutes = document.querySelector(".timer__input").value;
+    saved_link = localStorage.getItem("saved_link");
+    document.querySelector(".new_link_input").value = saved_link;
 
-            if (inputMinutes < 60) {
-                this.stop();
-                this.remainingSeconds = inputMinutes * 60;
-                this.updateInterfaceTime();
-            }
-        });
-
-        document.querySelector(".new_link_btn").addEventListener("click", () => {
-            document.querySelector(".embed_video").src = document.querySelector(".new_link_input")
-            .value;
-        });
+    if (saved_link.length != 0) {
+        document.querySelector(".embed_video").src = "//www.youtube.com/embed/" + getId(saved_link);
     }
 
-    updateInterfaceTime() {
-        const minutes = Math.floor(this.remainingSeconds / 60);
-        const seconds = this.remainingSeconds % 60;
+}
 
-        this.el.minutes.textContent = minutes.toString().padStart(2, "0");
-        this.el.seconds.textContent = seconds.toString().padStart(2, "0");
-    }
-
-    updateInterfaceControls() {
-        if (this.interval === null) {
-            this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-            this.el.control.classList.add("timer__btn--start");
-            this.el.control.classList.remove("timer__btn--stop");
-        } else {
-            this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
-            this.el.control.classList.add("timer__btn--stop");
-            this.el.control.classList.remove("timer__btn--start");
-        }
-    }
-
-    start() {
-        if (this.remainingSeconds === 0) return;
-        this.interval = setInterval(() => {
-            this.remainingSeconds--;
-            this.updateInterfaceTime();
-
-            if (this.remainingSeconds === 0) {
-                this.stop();
-                this.play();
-            }
-        }, 1000);
-        this.updateInterfaceControls();
-    }
-
-    stop() {
-        clearInterval(this.interval);
-        this.interval = null;
-        this.updateInterfaceControls();
-    }
-
-    play() {
-        var audio = new Audio("./alarm.wav");
-        audio.play();
+function refresh_Link() {
+    if (document.querySelector(".new_link_input").value.length == 0) {
+        return;
+    } else {
+        localStorage.setItem("saved_link", document.querySelector(".new_link_input").value);
+        saved_link = localStorage.getItem("saved_link");;
+        document.querySelector(".embed_video").src = "//www.youtube.com/embed/" + getId(saved_link);
     }
 }
 
-function start_timer(){
-    new Timer(document.querySelector(".timer"));
+function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return 'error';
+    }
 }
